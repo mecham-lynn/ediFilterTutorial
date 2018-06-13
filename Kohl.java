89910d9a348659dee958084ce02e3a9018324075
Lynn Mecham <mecham.lynn@gmail.com>
2018-06-12 22:31:15 -0600

    Lots of organization changes
    Added a few classes and started moving the methods and such.

---
+++ Kohl.java
@@ -0,0 +1,11 @@
+package ediFilterTutorial;
+
+public class Kohl{
+	
+Printer printer;
+	
+	public Kohl(Printer printer) {
+		this.printer = printer;
+	}
+
+}
---
+++ Printer.java
@@ -0,0 +1,36 @@
+package ediFilterTutorial;
+import java.util.ArrayList;
+import javax.swing.JTextArea;
+
+public class Printer {
+
+	JTextArea form;
+	
+	public Printer(JTextArea form) {
+		this.form = form;
+	}
+	public void printToForm(ArrayList<String> printData) {
+		for (int y = 0; y < printData.size(); y++) {
+			form.append("\n" + printData.get(y));
+		}
+	}
+	public void logError(String Error) {
+		System.out.println(Error);
+	}
+	
+	public void printMessageToForm(String message) {
+		form.setText(message);
+	}
+	
+	public void printDataToForm(String[] data, String segmentTerminator) {
+		
+		for (int i = 0; i < data.length; i++) {
+			//prints the data from the segments array and 
+			form.append(data[i] + segmentTerminator + "\n");
+		}
+	}
+	public void setForm(JTextArea form) {
+		this.form = form;
+	}
+
+}
